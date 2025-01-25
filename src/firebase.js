8427d977892ae71795ee007ee25c4a7a63aa3de1
bibakunin <ofatih-poyraz@hotmail.com>
2025-01-26 00:22:35 +0100

    deleted some components

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,25 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// Config değerlerini base64'e çevrilmiş halde saklayalım
+const _0x5f2d = [
+  'QUl6YVN5QkNsZXNpMkw1TzJxNTFzQkJFUFo4ZUNycl84S1haMmFV',  // apiKey
+  'bXktdHJhaW5pbmctdHJhY2tlci00MDE5NS5maXJlYmFzZWFwcC5jb20=',  // authDomain
+  'bXktdHJhaW5pbmctdHJhY2tlci00MDE5NQ==',  // projectId
+  'bXktdHJhaW5pbmctdHJhY2tlci00MDE5NS5hcHBzcG90LmNvbQ==',  // storageBucket
+  'ODU0NzMyMjU5MTM3',  // messagingSenderId
+  'MTo4NTQ3MzIyNTkxMzc6d2ViOjFjNTVmN2YzMGRkZTgwNjc5YTZiYzA=',  // appId
+  'Ry1MMVpLMVNIMVRS'   // measurementId
+];
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: atob(_0x5f2d[0]),
+  authDomain: atob(_0x5f2d[1]),
+  projectId: atob(_0x5f2d[2]),
+  storageBucket: atob(_0x5f2d[3]),
+  messagingSenderId: atob(_0x5f2d[4]),
+  appId: atob(_0x5f2d[5]),
+  measurementId: atob(_0x5f2d[6])
 };
 
 // Initialize Firebase
